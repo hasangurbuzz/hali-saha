2a0146012373a833c22d3c4e8becff83643c5845
hasangurbuzz <gurbuzhasann@yandex.com>
2022-05-04 08:24:40 +0300

    Form update

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,44 +1,44 @@
 import React, {useState} from 'react';
 import styled from "styled-components";
 
-const SignUpForm = (props) => {
-    const {status, toggler, addNewUser} = props
-    const [user, setUser] = useState({username: '', password: '', password_again: ''})
+const SignUpForm = ({status, toggler, addNewUser}) => {
+    const [inputUsername, setInputUsername] = useState('')
+    const [inputPassword, setInputPassword] = useState('')
+    const [inputPasswordAgain, setInputPasswordAgain] = useState('')
 
-    //check inputs are matching
-    const isPasswordsMatching = (user) => {
-        return user.password === user.password_again
+    const inputUsernameHandler = (e) => {
+        setInputUsername(e.target.value)
+    }
+    const inputPasswordHandler = (e) => {
+        setInputPassword(e.target.value)
+    }
+    const inputPasswordAgainHandler = (e) => {
+        setInputPasswordAgain(e.target.value)
     }
 
-    //send user data to parent component
-    const submitUser = (user) => {
-        if (isPasswordsMatching(user)) {
-            // addNewUser({username: user.username, password: user.password}) ? alert('kullanıcı adı alınmış') : alert('Kayıt tamamlandı')
-            if(addNewUser({username: user.username, password: user.password})){
-                alert('Kullanıcı adı alınmış')
-            }
-            else {
+    //check input passwords are matching
+    const arePasswordsMatching = () => {
+        return inputPassword === inputPasswordAgain
+    }
+    const generateUser = (username, password) => {
+        return {username: username, password: password}
+    }
+
+
+    const submitValues = (e) => {
+        e.preventDefault()
+        if (arePasswordsMatching()) {
+            if (addNewUser(generateUser(inputUsername, inputPassword))) {
+                alert("Kullanıcı adı alınmış")
+            } else {
                 toggler()
-                alert('kayıt tamamlandı')
+                alert("Kayıt tamamlandı")
             }
         } else {
             alert('Şifreler uyuşmuyor')
         }
-        //not necessary for state management
-        resetInputValue(document.getElementById('signupUsername'))
-        resetInputValue(document.getElementById('signupPassword'))
-        resetInputValue(document.getElementById('signupPasswordAgain'))
-        resetUserValue()
-    }
 
-    const resetInputValue = (e) => {
-        e.value = ""
-        resetUserValue()
     }
-    const resetUserValue = () => {
-        setUser({username: '', password: '',password_again: ''})
-    }
-
 
     return (
         <Container show={status}>
@@ -46,21 +46,25 @@ const SignUpForm = (props) => {
                 <button onClick={toggler}>close</button>
             </CloseButtonWrapper>
 
-            <InputLabel>Kullanıcı Adı</InputLabel>
-            <UsernameInput id={'signupUsername'} onChange={(e) => setUser({...user, username: e.target.value})}/>
+            <div>
+                <form onSubmit={submitValues}>
+                    <InputWrapper>
+                        <InputLabel>Kullanıcı Adı</InputLabel>
+                        <UsernameInput type={"text"} value={inputUsername} onChange={inputUsernameHandler}/>
+                    </InputWrapper>
+                    <InputWrapper>
+                        <InputLabel>Şifre</InputLabel>
+                        <PasswordInput value={inputPassword} onChange={inputPasswordHandler} type={'password'}/>
+                    </InputWrapper>
+                    <InputWrapper>
+                        <InputLabel>Şifre Tekrar</InputLabel>
+                        <PasswordInput value={inputPasswordAgain} onChange={inputPasswordAgainHandler}
+                                       type={'password'}/>
+                    </InputWrapper>
 
-            <InputLabel>Şifre</InputLabel>
-            <PasswordInput id={'signupPassword'} onChange={(e) => setUser({...user, password: e.target.value})} type={'password'}/>
-
-            <InputLabel>Tekrar Şifre</InputLabel>
-            <PasswordInput id={'signupPasswordAgain'}
-                onChange={(e) => setUser({...user, password_again: e.target.value})} type={'password'}
-                onKeyDown={(e) => {
-                    e.key === "Enter" && submitUser(user)
-                }}/>
-            <SubmitButton onClick={() => {
-                submitUser(user)
-            }}>Kayıt Ol</SubmitButton>
+                    <SubmitButton type="submit">Kayıt Ol</SubmitButton>
+                </form>
+            </div>
 
         </Container>
     );
@@ -77,7 +81,7 @@ const Container = styled.div`
   bottom: 0;
   right: 0;
   border: solid;
-  width: 200px;
+  width: 250px;
   z-index: 16;
   list-style: none;
   padding: 20px;
@@ -96,28 +100,36 @@ const Container = styled.div`
 const CloseButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  width: 200px;
+  width: 250px;
 
 `
 
 const InputLabel = styled.label`
+  font-size: 12px;
+  text-align: left;
+  width: 8vw;
 
 `
 
 const UsernameInput = styled.input`
-  margin: 5px;
+  margin: 5px 0 5px 5px;
   width: 15vw;
   align-items: center;
   border-radius: 10px;
   padding: 3px;
   min-width: 120px;
+  font-size: 12px;
 
-
-
+`
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
 
 const PasswordInput = styled(UsernameInput)`
 `
 
 const SubmitButton = styled.button`
+  margin-top: 5px;
 `
