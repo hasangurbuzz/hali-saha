1ab3810e57a5219d7dc6aa074ad3eadbe4f9fe4e
hasangurbuzz <gurbuzhasann@yandex.com>
2022-05-06 06:20:04 +0300

    attached Confirmation.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
 import styled from "styled-components";
-import Card from "./Card";
 import List from "./List";
 import Panel from "./Panel";
 
 
 const Home = (props) => {
-    const {imageData} = props
+    const {user, listOfHaliSaha,onAddReservationHandler} = props
 
-    const [user, setUser] = useState({username: 'admin'})
+
     const [inputText, setInputText] = useState('')
     const [panelStatus, setPanelStatus] = useState(false)
     const [chosenElement, setChosenElement] = useState({})
@@ -19,19 +18,13 @@ const Home = (props) => {
         setInputText(lowerCase);
     };
 
-    const listOfHalisaha = [
-        {id: '1', name: 'edirne halı saha', image: 'hali-saha-1.jpg',address: 'Edirne', tel: '01234567891'},
-        {id: '2', name: 'Balkan halı saha', image: 'hali-saha-2.jpg',address: 'Edirne', tel: '01234567892'},
-        {id: '3', name: 'Ayşekadın halı saha', image: 'hali-saha-3.jpg',address: 'Edirne', tel: '01234567893'},
-        {id: '4', name: 'Selimiye halı saha', image: 'hali-saha-4.jpg',address: 'Edirne', tel: '01234567894'},
-        {id: '5', name: 'Sarayiçi halı saha', image: 'hali-saha-5.jpg',address: 'Edirne', tel: '01234567895'},
-        {id: '6', name: 'Şükrüpaşa halı saha', image: 'hali-saha-6.jpg',address: 'Edirne', tel: '01234567896'},
-    ]
+    const onConfirmationHandler = ()=>{
+        onAddReservationHandler(chosenElement)
+    }
+
 
     const panelStatusHandler = () => {
         setPanelStatus(!panelStatus)
-
-
     }
 
     const chosenElementHandler = (element) => {
@@ -41,36 +34,26 @@ const Home = (props) => {
 
     return (
         <Container>
-            {/*<h1>Halı Sahalar</h1>*/}
-
-
             {panelStatus &&
-                <Panel panelStatusHandler={panelStatusHandler} chosenElement={chosenElement}
-                       />}
+                <Panel user={user} panelStatusHandler={panelStatusHandler} chosenElement={chosenElement}
+                       onConfirmationHandler={onConfirmationHandler}
+                />}
 
             {!panelStatus &&
                 <>
-
-
                     <SearchBarWrapper>
                         <SearchBar value={inputText ?? ""} type={'text'} id={'searchbar'} placeholder={'Ara'}
                                    onChange={inputHandler}/>
                         <ClearButton onClick={() => setInputText('')}>Temizle</ClearButton>
                     </SearchBarWrapper>
 
-                    <List listData={listOfHalisaha} searchInput={inputText} chosenElementHandler={chosenElementHandler}
+                    <List listData={listOfHaliSaha} searchInput={inputText} chosenElementHandler={chosenElementHandler}
                           panelStatusHandler={panelStatusHandler}/>
 
                 </>
             }
 
 
-            {!user.username &&
-                <ImageWrapper>
-                    {imageData.map((data, index) =>
-                        <Card image={data.image} description={data.description} key={index}/>
-                    )}
-                </ImageWrapper>}
         </Container>
     );
 };
@@ -87,26 +70,20 @@ const Container = styled.div`
 
 `
 
-const ImageWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 100px 0 100px;
-
-`
 
 const SearchBar = styled.input`
-    font-size: 14px;
-  
+  font-size: 14px;
+
 
 `
 const SearchBarWrapper = styled.div`
   display: flex;
-  
+
 
 `
 const ClearButton = styled.button`
-  
+
   border: solid 1px;
   border-radius: 2px;
-  
+
 `
