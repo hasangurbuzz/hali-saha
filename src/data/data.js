d14c898af35b0f541f0c737b80ce166b129b134d
hasangurbuzz <gurbuzhasann@yandex.com>
2022-05-07 19:11:49 +0300

    data parceled

---
+++ src/data/data.js
@@ -0,0 +1,60 @@
+export default [
+    {
+        id: '1',
+        name: 'edirne halı saha',
+        image: 'hali-saha-1.jpg',
+        address: 'Edirne',
+        tel: '01234567891',
+        chosenDates: [{
+            time: ['11', '10'],
+            date: '2022-05-06',
+            username: 'admin'
+        },
+            {
+                time: ['12', '13', '14'],
+                date: '2022-05-07',
+                username: 'admin'
+            }
+        ]
+    },
+    {
+        id: '2',
+        name: 'Balkan halı saha',
+        image: 'hali-saha-2.jpg',
+        address: 'Edirne',
+        tel: '01234567892',
+        chosenDates: []
+    },
+    {
+        id: '3',
+        name: 'Ayşekadın halı saha',
+        image: 'hali-saha-3.jpg',
+        address: 'Edirne',
+        tel: '01234567893',
+        chosenDates: []
+    },
+    {
+        id: '4',
+        name: 'Selimiye halı saha',
+        image: 'hali-saha-4.jpg',
+        address: 'Edirne',
+        tel: '01234567894',
+        chosenDates: []
+    },
+    {
+        id: '5',
+        name: 'Sarayiçi halı saha',
+        image: 'hali-saha-5.jpg',
+        address: 'Edirne',
+        tel: '01234567895',
+        chosenDates: []
+    },
+    {
+        id: '6',
+        name: 'Şükrüpaşa halı saha',
+        image: 'hali-saha-6.jpg',
+        address: 'Edirne',
+        tel: '01234567896',
+        chosenDates: []
+    },
+]
